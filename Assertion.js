3dfc0978e1541eef58b5355e56409963d4712563
Cefi Menda <jeffymenda@gmail.com>
2020-05-17 23:46:17 -0400

    fix issue where error wasn't expected to be thrown

--- Assertion.js
+++ Assertion.js
@@ -28,7 +28,7 @@ module.exports = class Assertion {
     if (this.assertions) { this.assertions() };
   };
 
-  throw = error => expect(this._obj.bind(this, ...this.args)).to.throw();
+  throw = error => expect(this._obj.bind({}, ...this.args)).to.throw(error);
 
   loosely = () => {
     this.loosely = true;
